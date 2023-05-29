4be7d1ae9754a69483bacb775a25404e6c878695
shaha <shahafwieder@campus.technion.ac.il>
2023-05-29 20:29:28 +0300

    COMMIT 4

--- src/Difference.java
+++ src/Difference.java
@@ -1,66 +1,65 @@
-public class Sum extends Function {
+public class Difference extends Function {
     private double [] values1;
     private double [] values2;
-    private double[] sumValue;
-    public Sum(double[] p1, double[] p2) {
+    private double[] diffValues;
+    public Difference(double[] p1, double[] p2) {
         this.values1 = p1;
         this.values2 = p2;
-        sumValue=new double[Math.max(this.values1.length,this.values2.length)];
+        diffValues =new double[Math.max(this.values1.length,this.values2.length)];
         int counter = 0;
         for(int i=0;i<Math.min(this.values1.length,this.values2.length);i++){
-            sumValue[i]=values1[i]+values2[i];
+            diffValues[i]=values1[i]-values2[i];
             counter ++;
         }
         for(int j = counter; j<Math.max(this.values1.length,this.values2.length);j++){
-           if(this.values1.length >= this.values2.length){
-               sumValue[j]=values1[j];
-           }
-           else{
-               sumValue[j]=values2[j];
-           }
+            if(this.values1.length >= this.values2.length){
+                diffValues[j]=values1[j];
+            }
+            else{
+                diffValues[j]=values2[j];
+            }
         }
     }
-    @Override
     public double valueAt(double x){
         double value = 0;
-        for(int i = 0; i<this.sumValue.length; i++){
-            value += this.sumValue[i] * Math.pow(x, i);
+        for(int i = 0; i<this.diffValues.length; i++){
+            value += this.diffValues[i] * Math.pow(x, i);
         }
         return value;
     }
     @Override
     public String toString(){
         String polyString = "";
-        for(int i = 0; i<this.sumValue.length; i++) {
+        for(int i = 0; i<this.diffValues.length; i++) {
             if (i == 0) {
-                if (checkInt(this.sumValue[i])){
-                    polyString+=(int)this.sumValue[i];
+                if (checkInt(this.diffValues[i])){
+                    polyString+=(int)this.diffValues[i];
                 }
                 else{
-                    polyString+=this.sumValue[i];
+                    polyString+=this.diffValues[i];
                 }
             }
             else {
-                if (this.sumValue[i] > 0) {
+                if (this.diffValues[i] > 0) {
                     polyString += " + ";
                 } else {
                     polyString += " - ";
                 }
-                if (this.sumValue[i] != 1 || this.sumValue[i] != -1) {
+                if (this.diffValues[i] != 1 || this.diffValues[i] != -1) {
                     if (i == 1) {
-                        if (checkInt(this.sumValue[i])){
-                            polyString += (int) this.sumValue[i] + "x";
+                        if (checkInt(this.diffValues[i])){
+                            polyString += (int) this.diffValues[i] + "x";
                         }
                         else {
-                            polyString+=this.sumValue[i]+ "x";
+                            polyString+=this.diffValues[i]+ "x";
                         }
                     }
                     else {
-                        if (checkInt(this.sumValue[i])){
-                            polyString += (int) this.sumValue[i] + "x^" + i;
+                        if (checkInt(this.diffValues[i])){
+                            polyString += (int) this.diffValues[i] + "x^" + i;
                         }
                         else {
-                            polyString+=this.sumValue[i]+"x^" + i;
+                            polyString+=this.diffValues[i]+"x^" + i;
                         }
                     }
                 }
@@ -77,10 +76,10 @@ public class Sum extends Function {
     }
     @Override
     public Function derivative(){
-        if(sumValue.length > 1) {
-            double[] derivativeCoefficients = new double[sumValue.length - 1];
-            for (int i = 1; i < sumValue.length; i++) {
-                derivativeCoefficients[i - 1] = sumValue[i] * i;
+        if(diffValues.length > 1) {
+            double[] derivativeCoefficients = new double[diffValues.length - 1];
+            for (int i = 1; i < diffValues.length; i++) {
+                derivativeCoefficients[i - 1] = diffValues[i] * i;
             }
             return new Polynomial(derivativeCoefficients);
         }
@@ -98,7 +97,8 @@ public class Sum extends Function {
             return false;
         }
     }
-    public double [] getSumValues(){
-        return this.sumValue;
+    public double [] getDiffValues(){
+        return this.diffValues;
     }
 }
+
--- src/MultiSum.java
+++ src/MultiSum.java
@@ -1,10 +1,12 @@
-public class MultiSum extends Function{
-    private Function [] values;
+public class MultiSum extends Polynomial{
+    private Polynomial[] values;
     private  double [] multiSumFunction;
-    public MultiSum(Function...p) {
+    public MultiSum(Polynomial...p) {
             this.values = p;
             for(int i = 0; i< this.values.length-1; i++){
-                this.multiSumFunction=new Sum(this.values[i].getValues(),this.values[i+1].getValues()).;
+                this.multiSumFunction=new Sum(this.values[i].getValues(),this.values[i+1].getValues()).getSumValues();
             }
     }
+
+
 }
